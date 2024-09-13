3e00b5bf87e3e869b370d72bb9ccfa105e1b370e
Alekssandher <alekssandher1@hotmail.com>
2024-09-13 12:40:23 -0300

    updates

---
+++ systems/doors/entrance_doors.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+@export var animationPlayer: AnimationPlayer
+var doorOpened = false
+var areaIn = false
+
+func _process(delta: float) -> void:
+	if !areaIn: return
+	
+	if Input.is_action_just_pressed("leftClick") and !animationPlayer.is_playing():
+		if !doorOpened:
+			animationPlayer.play("open")
+			doorOpened = true
+		
+		else:
+			animationPlayer.play("close")
+			doorOpened = false
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	
+	areaIn = true
+			
+
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	areaIn = false
