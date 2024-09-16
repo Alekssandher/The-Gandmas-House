56d1a84b968f72547f2e8155c735baf11cb46240
Alekssandher <alekssandher1@hotmail.com>
2024-09-16 12:52:23 -0300

    updates

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -147,3 +147,8 @@ func checkStammina():
 		canRegenStammina = true
 		canRun = true
 		
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("player"):
+		print("Body entered")
