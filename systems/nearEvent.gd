d36d5659afccd324a32a8c68f8635dad48172885
Alekssandher <alekssandher1@hotmail.com>
2024-10-03 11:08:23 -0300

    Created class to interact

---
+++ systems/nearEvent.gd
@@ -0,0 +1,48 @@
+extends Node
+
+class_name ObjectInteract
+
+@export_category("Player")
+@export var player: CharacterBody3D
+
+@export_category("Object")
+@export var object: MeshInstance3D
+
+@export_category("Params")
+@export var texts: Array[String] = []
+@export var interactDistance: float = 1.2
+@export var disToStopText: float
+@export var canShowText: bool = true
+
+var playerDirection: Vector3
+var directionToObject: Vector3
+var dotProduct: float
+var distanceToPlayer: float
+
+var i: int = 0
+
+
+func _process(delta: float) -> void:
+	
+	if looking() and distanceToPlayer < interactDistance:
+		if !canShowText: return
+		showText()
+
+func showText() -> void:
+	canShowText = false
+	while i < texts.size():
+		if distanceToPlayer > disToStopText: return;
+		await World.typingEffect(texts[i], 0.1, 1.5)
+		i += 1
+	
+func looking() -> bool:
+	#Calcs distance to player
+	distanceToPlayer = object.global_position.distance_to(player.global_position)
+	
+	#Calcs if the player and object are in front of each other
+	playerDirection = -player.global_transform.basis.z.normalized()
+	directionToObject = (object.global_position - player.global_position).normalized()
+	dotProduct = playerDirection.dot(directionToObject)
+	
+	#Returns the result of the calcs with a error margin
+	return dotProduct > 0.8
